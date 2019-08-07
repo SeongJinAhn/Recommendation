670a5856401555e4a4a14cca13a7ee4d69584820
sssjjjaaa <37531907+SeongJinAhn@users.noreply.github.com>
2019-08-07 22:18:40 +0900

    add GPU, evaluation

--- group-recommendation/main.py
+++ group-recommendation/main.py
@@ -1,18 +1,8 @@
-'''
-Created on Nov 10, 2017
-Main function
-
-@author: Lianhai Miao
-'''
-from model.agree import AGREE
 import torch
 import torch.nn as nn
-import torch.autograd as autograd
-from torch.autograd import Variable
 import torch.optim as optim
-import torch.nn.functional as F
+from model.agree import AGREE
 import numpy as np
-from time import time
 from config import Config
 from utils.util import Helper
 from dataset import GDataset
@@ -26,21 +16,23 @@ def training(model, train_loader, epoch_id, config, type_m):
     losses = 0
     for batch_id, (u, pi_ni) in enumerate(train_loader):
         # Data Load
+        if config.GPU_available == True:
+            u = u.to(device)
+            pi_ni = pi_ni.to(device)
+
         user_input = u
-        pos_item_input = pi_ni[:, 0]
-        neg_item_input = pi_ni[:, 1]
+        pos_item, neg_item = pi_ni[:,0], pi_ni[:,1]
         # Forward      모델을 2개돌리고 합쳐서 loss로 한다
         if type_m == 'user':
-            pos_pred = model(None, user_input, pos_item_input)
-            neg_pred = model(None, user_input, neg_item_input)
+            pos_pred = model(None, user_input, pos_item)
+            neg_pred = model(None, user_input, neg_item)
         elif type_m == 'group':
-            pos_pred = model(user_input, None, pos_item_input)
-            neg_pred = model(user_input, None, neg_item_input)
+            pos_pred = model(user_input, None, pos_item)
+            neg_pred = model(user_input, None, neg_item)
         # Zero_grad
         model.zero_grad()
         # Loss
         loss = torch.mean((pos_pred - neg_pred -1) **2)
-        # record loss history
         losses += float(loss)  
         # Backward
         loss.backward()
@@ -48,6 +40,12 @@ def training(model, train_loader, epoch_id, config, type_m):
 
     losses /= batch_id
     print('Iteration %d, loss is [%.4f ]' % (epoch_id, losses))
+
+def evaluation(model, helper, testRatings, testNegatives, K, type_m):
+    model.eval()
+    (hits, ndcgs) = helper.evaluate_model(model, testRatings, testNegatives, K, type_m)
+    hr, ndcg = np.array(hits).mean(), np.array(ndcgs).mean()
+    return hr, ndcg
 
 if __name__ == '__main__':
     config = Config()
@@ -62,16 +60,20 @@ if __name__ == '__main__':
     num_users, num_items = dataset.num_users, dataset.num_items
 
     # build AGREE model
-    agree = AGREE(num_users, num_items, num_group, config.embedding_size, g_m_d, config.drop_ratio)
+    agree = AGREE(num_users, num_items, num_group, config.embedding_size, g_m_d, config.drop_ratio, config.GPU_available)
 
     # config information
     print("AGREE at embedding size %d, run Iteration:%d, NDCG and HR at %d" %(config.embedding_size, config.epoch, config.topK))
     # train the model
+    if config.GPU_available:
+        device = torch.device('cuda')
+        agree = agree.to(device)
+
     for epoch in range(config.epoch):
         agree.train()
-        # training
-        t1 = time()
         training(agree, dataset.get_user_dataloader(config.batch_size), epoch, config, 'user')
         training(agree, dataset.get_group_dataloader(config.batch_size), epoch, config, 'group')
-        print("user and group training time is: [%.1f s]" % (time()-t1))
+
+        u_hr, u_ndcg = evaluation(agree, helper, dataset.user_testRatings, dataset.user_testNegatives, config.topK, 'user')
+        hr, ndcg = evaluation(agree, helper, dataset.group_testRatings, dataset.group_tyestNegatives, config.topK, 'group')
     print("Done!")
